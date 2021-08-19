904bcae60d0979889a7a76e6878aa8b459d60df7
tusharrao198 <tushar.rao198@gmail.com>
2021-08-19 14:24:55 +0530

    solved leetcode & interviewbit  DP questions 1. Edit Distance 2. Unique Paths 3. Distinct Subsequence 4. Plaindrome Partitioning 5. Perfect Squares (fastest Method O(sqrt(n))

---
+++ practice_questions/leetcode/DP/uniquePaths.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/unique-paths/submissions/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        dp = [[-1 for i in range(n+1)] for _ in range(m+1)]
+        
+        for i in range(m+1):
+            dp[i][0] = 1
+
+        for i in range(n+1):
+            dp[0][i] = 1
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):  
+                      
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
+    
+        
+        
